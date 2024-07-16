d2c5e4d0ad9deab63aa451fb8458e57d129a10d9
XIUPING ZOU <xiaoz93@outlook.com>
2024-07-16 18:28:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,8 +9,7 @@ run() {
     echo "nameserver 119.29.29.29" > /etc/resolv.conf
     echo "nameserver 223.5.5.5" >> /etc/resolv.conf
     # 后台运行Zdir
-    nohup ./zdir start > data/logs/access.log 2>&1 &
-    tail -n 500 -f data/logs/access.log
+    ./zdir start
     # 判断架构
     # get_arch=$(arch)
     # if [[ "${get_arch}" == "x86_64" ]]
