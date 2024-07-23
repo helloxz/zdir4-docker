67a960cfbc5452e742ba9482d804de9cd590f7ec
XIUPING ZOU <xiaoz93@outlook.com>
2024-07-23 15:18:01 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ run() {
     cd /opt/zdir/
     chmod +x sh/*.sh
     # 设置DNS
-    echo "nameserver 119.29.29.29" > /etc/resolv.conf
-    echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+    echo "nameserver 223.5.5.5" > /etc/resolv.conf
+    echo "nameserver 119.29.29.29" >> /etc/resolv.conf
     # 后台运行Zdir
     ./zdir start
     # 判断架构
