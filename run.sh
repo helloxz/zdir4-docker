7284576b1211f7566b530351086efb27c8805dfc
XIUPING ZOU <xiaoz93@outlook.com>
2025-02-07 14:32:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,26 @@
 
 BASE_DIR="/opt/zdir"
 
+# 升级处理
+upgrade() {
+    cd $BASE_DIR
+    # 检查并删除指定文件
+    if ls $BASE_DIR/zdir_*.tar.gz 1> /dev/null 2>&1; then
+        # 删除不需要的文件
+        rm -rf $BASE_DIR/zdir
+        rm -rf $BASE_DIR/templates/assets/default/*
+        # 覆盖解压
+        tar -xzvf $BASE_DIR/zdir_*.tar.gz -C $BASE_DIR
+        # 添加执行权限
+        chmod +x $BASE_DIR/zdir
+        # 清理不需要的文件
+        rm -f $BASE_DIR/zdir_*.tar.gz
+    fi
+}
+
 # 运行zdir
 run() {
-    cd /opt/zdir/
+    cd $BASE_DIR
     chmod +x sh/*.sh
     # 设置DNS
     echo "nameserver 223.5.5.5" > /etc/resolv.conf
